be2f5e9fd3302f8ab0949ade66634b4792a35892
elymatos <ely.matos@gmail.com>
2020-09-11 19:51:07 -0300

    adding port parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ execute_ssh(){
   echo "Execute Over SSH: $@"
   echo "Host: $INPUT_REMOTE_DOCKER_HOST $INPUT_REMOTE_DOCKER_PORT -"
 #  ssh -q -t -i "$HOME/.ssh/id_rsa" -p "$INPUT_REMOTE_DOCKER_PORT" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$INPUT_REMOTE_DOCKER_HOST" "$@"
-  ssh -t -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" framenetbr@200.131.61.130:8090 "$@"
+  ssh -t -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -J framenetbr@200.131.61.130:8090 "$@"
 }
 
 if [ -z "$INPUT_REMOTE_DOCKER_HOST" ]; then
